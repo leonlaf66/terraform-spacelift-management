633edd97f0da7fdcac0b8b296eb83f08e9a4da5e
Leon <leonlaf66@gmail.com>
2025-09-17 16:38:43 -0400

    init the project

--- dev/main.tf
+++ dev/main.tf
@@ -28,6 +28,6 @@ output "stack_count" {
 
 resource "spacelift_stack" "test" {
   name       = "test-stack-minimal"
-  repository = "spacelift-demo"
+  repository = "leonlaf66/spacelift-demo"
   branch     = "main"
 }
--- dev/versions.tf
+++ dev/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     spacelift = {
       source  = "spacelift-io/spacelift"
-      version = "~> 1.33.0"
+      version = "1.32.0"
     }
   }
 }
