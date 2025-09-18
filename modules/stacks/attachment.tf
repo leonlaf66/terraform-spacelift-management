23beadf9ad8b43274c17e146d8863f1e13b6b1f6
Leon <leonlaf66@gmail.com>
2025-09-18 18:52:25 -0400

    spacelift deployed stack test

--- modules/stacks/attachment.tf
+++ modules/stacks/attachment.tf
@@ -20,13 +20,3 @@ resource "spacelift_aws_integration_attachment" "this" {
   read           = var.aws_attachment_read
   write          = var.aws_attachment_write
 }
-
-output "debug_info" {
-  value = {
-    stack_id                      = spacelift_stack.app.id
-    policy_ids_to_attach          = var.policy_ids_to_attach
-    aws_integration_ids_to_attach = var.aws_integration_ids_to_attach
-    policy_attachments_created    = keys(spacelift_policy_attachment.this)
-    aws_attachments_created       = keys(spacelift_aws_integration_attachment.this)
-  }
-}
