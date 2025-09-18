5627c54b6b5ceae18bb20dbb5a0be4bae877a303
Leon <leonlaf66@gmail.com>
2025-09-17 20:42:33 -0400

    test

--- dev/providers.tf
+++ dev/providers.tf
@@ -1,2 +1,5 @@
 provider "spacelift" {
+  api_key_endpoint = "https://leon-test.app.spacelift.io"
+  api_key_id       = "01K5CB2ARR7EA4QK8AAVY67CP6"
+  api_key_secret   = "710237e49988025875672b2dcf72129fa36631f3ea28614d9da045853faf48e8"
 }
