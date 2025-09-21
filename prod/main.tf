c4148720eeff7179987e628e3ec6cfe4ccf44269
Leon <leonlaf66@gmail.com>
2025-09-21 12:55:31 -0400

    deploy prod

---
+++ prod/main.tf
@@ -0,0 +1,22 @@
+module "spacelift_stacks" {
+  source = "../modules/stacks"
+  for_each                 = local.stacks
+  stack_name               = "${each.key}-${local.environment}"
+  repository               = each.value.repository
+  branch                   = local.prod_branch
+  space_id                 = local.prod_space_id
+  project_root             = each.value.project_root
+  description              = each.value.description
+  environment              = local.environment
+  additional_project_globs = each.value.additional_project_globs
+  github_action_deploy     = local.github_action_deploy
+
+  common_labels = merge(
+    local.common_labels,
+    { "app" = each.key }
+  )
+
+  policy_ids_to_attach          = lookup(each.value, "policy_ids_to_attach", [])
+  context_ids_to_attach         = lookup(each.value, "context_ids_to_attach", [])
+  aws_integration_ids_to_attach = lookup(each.value, "aws_integration_ids_to_attach", [])
+}
