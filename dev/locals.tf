4eb578771c50dce095fd0898ec3f047f3e93878c
Leon <leonlaf66@gmail.com>
2025-11-10 13:42:54 -0500

    deploy the job

--- dev/locals.tf
+++ dev/locals.tf
@@ -30,6 +30,7 @@ locals {
       description                   = "Manages dev infrastructure for nodejs codebuild project."
       repository                    = "terraform-codebuild-demo"
       project_root                  = local.environment
+      additional_project_globs      = []
       aws_integration_ids_to_attach = [local.dev_aws_integration_id]
     }
   }
